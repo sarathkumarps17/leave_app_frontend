daaae8d317cc146af4cd94d45aa2f9b65054d9f5
syam <syamvishnu@gmail.com>
2021-02-15 18:50:10 +0530

    syam

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import LeftMenu from "../Layouts/LeftMenu";
+import CardExampleLinkCard from "./Homecard";
+
 
 const Home = () => {
   return (
     <div>
       <LeftMenu />
+      
       Home
+      <CardExampleLinkCard />
     </div>
   );
 };
